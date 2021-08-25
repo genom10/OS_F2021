029cce34a4ebd7d43a2ba85624d7a67b81734253
Egor <genom_10@mail.ru>
2021-08-25 14:01:51 +0300

    ex2 fix

--- week2/ex2.c
+++ week2/ex2.c
@@ -1,21 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
-void swap(int &a, int &b){
-	int temp = a;
-	a = b;
-	b = temp;
-}
-
 int main(){
-	printf("Input values to swap:\n");
-	int a, b;
-	printf("a:");
-	scanf("%i", &a);
-	printf("b:");
-	scanf("%i", &b);
-	printf("before swap a:%i, b:%i", a, b);
-	swap(a, b);
-	printf("after swap a:%i, b:%i", a, b);
+	printf("Input string to reverse:\n");
+	char str[100];
+	scanf("%s", str);
+	printf("Reversed string:\n");
+	for (int i = strlen(str)-1; i >= 0; i--)
+		printf("%c",str[i]);
 	return 0;
 }
