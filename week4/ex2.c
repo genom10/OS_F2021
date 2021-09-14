d5bf876c03057479bdbefb18a2502c5d9fbe6bff
Genom10 <genom_10@mail.ru>
2021-09-14 21:57:44 +0300

    Clarification for week4/ex2.c

--- week4/ex2.c
+++ week4/ex2.c
@@ -4,8 +4,8 @@
 int main(int argc, char *argv[]){
 	int n = atoi(argv[1]);
 	int pid;
-	// for each itteration of the loop n childs will be created,
-	// then for all childrens loop continues with i copied from the moment of creation of the child.
+	// for each itteration of the loop a child will be forked with current value of i,
+	// then for all childrens loop continues by creating i-1 children.
 	// due to that 1st 1st-gen child will create n-1 children, while 2nd 1-st gen child will create n-2 children.
 	for (int i = 0; i < n; i++){
 		pid = fork();
