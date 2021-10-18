71bce3ddc09eeb418d76c67f9a4de0c3fc0e3f9c
Egor <genom_10@mail.ru>
2021-10-18 19:12:10 +0300

    week9

---
+++ week9/ex1.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct page{
+    unsigned short age;
+    unsigned short id;
+}page;
+
+
+page *pages;
+int n;
+
+void printPages(){
+    for (int i = 0; i < n; i++){
+        printf("i: %i\tid: %hu\tage: %hu\t\t%hx\n", i, pages[i].id, pages[i].age, pages[i].age);
+    }
+}
+
+int main()
+{
+    scanf("%i", &n);
+    pages = malloc(n*sizeof(page));
+    for (int i = 0; i < n; i++){
+        pages[i].id = -1;
+        pages[i].age = 0;
+    }
+    
+    int misses = 0;
+    int hits=0;
+    FILE * inp = fopen("input.txt", "r");
+    unsigned short request;
+    while (fscanf(inp, "%hu", &request) != EOF){
+        unsigned short minAge = -1;
+        unsigned short minAgeN = -1;
+        int pageFault = 1;
+        for (int i = 0; i < n; i++){
+            if (pages[i].id == request){
+                minAgeN = i;
+                pageFault = 0;
+                break;
+            }
+            minAgeN = (minAge > pages[i].age ? i : minAgeN);
+            minAge = (minAge > pages[i].age ? pages[i].age : minAge);
+        }
+        
+        for (int i = 0; i < n; i++){
+            pages[i].age >>= 1;
+        }
+        
+        if (pageFault){
+            pages[minAgeN].id = request;
+            pages[minAgeN].age = 32768;
+            misses++;
+        }else{
+            pages[minAgeN].age += 32768;
+            hits++;
+        }
+        
+        //printPages();
+        printf("page %i was added.\n\n", request);
+    }
+    
+    printf("hit ratio: %f %%", ((float)hits)/(hits+misses)*100);
+    return 0;
+}
