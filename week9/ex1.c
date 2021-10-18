e8a58b69b7cc613273ba67b0ba79549655d39256
Egor <genom_10@mail.ru>
2021-10-18 21:24:33 +0300

    ex1.c update

--- week9/ex1.c
+++ week9/ex1.c
@@ -21,15 +21,14 @@ int main()
     scanf("%i", &n);
     pages = malloc(n*sizeof(page));
     for (int i = 0; i < n; i++){
-        pages[i].id = -1;
         pages[i].age = 0;
     }
     
     int misses = 0;
     int hits=0;
     FILE * inp = fopen("input.txt", "r");
-    unsigned short request;
-    while (fscanf(inp, "%hu", &request) != EOF){
+    int request;
+    while (fscanf(inp, "%i", &request) != EOF){
         unsigned short minAge = -1;
         unsigned short minAgeN = -1;
         int pageFault = 1;
@@ -49,17 +48,17 @@ int main()
         
         if (pageFault){
             pages[minAgeN].id = request;
-            pages[minAgeN].age = 32768;
+            pages[minAgeN].age = 0x8000;
             misses++;
         }else{
-            pages[minAgeN].age += 32768;
+            pages[minAgeN].age += 0x8000;
             hits++;
         }
         
         //printPages();
-        printf("page %i was added.\n\n", request);
+        //printf("page %i was added.\n\n", request);
     }
     
-    printf("hit ratio: %f %%", ((float)hits)/(hits+misses)*100);
+    printf("%i hits : %i misses\nhit ratio: %f %%", hits, misses, ((float)hits)/(hits+misses)*100);
     return 0;
 }
